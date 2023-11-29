4c1ba08b6c447096df9c975758f46442ac4f4f00
Jona Goldwasser <pingu007@gmail.com>
2023-11-30 09:11:55 +1100

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    let array = arguments[0];
+    for (let i = 1; i < arguments.length; i++)
+    {
+        let toRemove = arguments[i];
+        let index = array.indexOf(toRemove);
+        if (index >= 0) {
+            array.splice(index, 1);
+        }   
+    }
+    return array;
 };
 
 // Do not edit below this line
