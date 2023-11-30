782c10f9062d28537a1554ea522e224265f0587b
Jona Goldwasser <pingu007@gmail.com>
2023-12-01 08:28:19 +1100

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if (!Number.isInteger(end) || !Number.isInteger(start))
+    {
+        return("ERROR");
+    }
+    if (end < 0 || start < 0) {
+        return("ERROR");
+    }
+    if (end < start) {
+        let temp = end;
+        end = start;
+        start = temp;
+    }
+    for (i = start; i <= end; i++)
+    {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
