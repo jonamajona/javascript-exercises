5a8323cfd84bb2e73d9e01c6e08be926f1bea748
Jona Goldwasser <pingu007@gmail.com>
2023-12-12 13:13:26 +1100

    complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arrayToSum) {
+	return arrayToSum.reduce(
+    (previousValue, currentValue) => previousValue + currentValue, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arrayToMultiply) {
+  return arrayToMultiply.reduce(
+    (previousValue, currentValue) => previousValue * currentValue, 1)
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+  for (let i = num; i > 0; i--)
+  {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
