0370d0799dbe4baf9c312c97fdd073f92b432760
Jona Goldwasser <pingu007@gmail.com>
2023-12-14 10:13:08 +1100

    complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) { 
+    if(parseInt(num) < 0) {
+        return "OOPS";
+    }
+    let fibLast = 0;
+    let fibSecondLast = 0;
+    let fibNext = 1;
+    for(i=0; i<parseInt(num); i++) {
+        fibLast = fibNext;
+        fibNext = fibLast + fibSecondLast;
+        fibSecondLast = fibLast;
+    }
+    return fibLast;
 };
 
 // Do not edit below this line
