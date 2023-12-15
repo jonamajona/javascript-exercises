d736b3840d8e1adc27b0a3f9bdd09f10925c58c1
Jona Goldwasser <pingu007@gmail.com>
2023-12-15 13:11:03 +1100

    complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce(
+        (prev, current) => {
+            const d = new Date();
+            let year = d.getFullYear();
+            if (!prev.yearOfDeath) {
+                prev.yearOfDeath = year;
+            }
+            if (!current.yearOfDeath) {
+                current.yearOfDeath = year;
+            }
+            return (prev.yearOfDeath - prev.yearOfBirth) > (current.yearOfDeath - current.yearOfBirth) ? prev : current
+        }
+    )
 };
 
 // Do not edit below this line
